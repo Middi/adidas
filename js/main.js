aaecd70a24319f3333e17a44b5bb630ba504eeab
“Richard Middleton” <Richard@richardmiddleton.me>
2017-08-29 22:39:38 +0100

    nav active selection JS and CSS

--- js/main.js
+++ js/main.js
@@ -19,4 +19,9 @@ $(document).ready(function () {
             });
         }
     });
+
+    $('.nav-link').on('click', function(){
+        $('.nav-link').removeClass('nav-link-active');
+        $(this).addClass('nav-link-active');
+    });
 });
