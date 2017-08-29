8389da78955abb4a7d18a2e006083f5eb7215bf6
“Richard Middleton” <Richard@richardmiddleton.me>
2017-08-29 22:42:12 +0100

    tidy

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ $(document).ready(function () {
         }
     });
 
+    
     $('.nav-link').on('click', function(){
         $('.nav-link').removeClass('nav-link-active');
         $(this).addClass('nav-link-active');
