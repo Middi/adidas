5b5215fe8c2cd5273c86181e8b5774c1de415f93
“Richard Middleton” <Richard@richardmiddleton.me>
2017-08-29 22:26:52 +0100

    font awesome menu icons

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@ $(document).ready(function () {
 
     $('.hamburger').click(function () {
         if ($('.mobile-nav').hasClass('hidden')) {
+            $('.fa').removeClass('fa-bars').addClass('fa-times');
             $('.mobile-nav').slideDown("slow", function () {
                 $('.mobile-nav').removeClass('hidden');
             });
-        } else {
+        } 
+        
+        else {
+            $('.fa').removeClass('fa-times').addClass('fa-bars');
             $('.mobile-nav').slideUp("slow", function () {
                 $('.mobile-nav').addClass('hidden');
             });
